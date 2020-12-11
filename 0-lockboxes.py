ac2bab3e48853d80fc69fbab74bc46fe35b2b7fc
Diego <vandeldiegoc@gmail.com>
2020-12-10 23:37:02 -0500

    0[add]

---
+++ 0-lockboxes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''module'''
+
+
+def canUnlockAll(boxes):
+    """ funtion that determines if all the boxes can be opene """
+    openbox = [0]
+    for key in openbox:
+        for box in boxes[key]:
+            if box not in openbox and box < len(boxes):
+                openbox.append(box)
+    if len(openbox) == len(boxes):
+        return True
+    else:
+        return False
