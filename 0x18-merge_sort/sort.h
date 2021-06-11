4e554c0687caa10cad7dcd338e6c41e665c700c7
vandel <vandeldiegoc@gmail.com>
2021-06-10 19:55:37 -0500

    add[0]

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -2,7 +2,6 @@
 #define _SORT_H
 #include <stdio.h>
 
-void final_merge(int *array, int *left, int *right, size_t size);
 void merge_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 
