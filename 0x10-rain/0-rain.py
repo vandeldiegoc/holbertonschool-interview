6f2e6864c16dcd1654d8f96001963cea9f2a9661
vandel <vandeldiegoc@gmail.com>
2021-06-16 16:37:18 -0500

    0[add]

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def rain(walls):
+    """calculate how many square
+       units of water will be retained
+       after it rains """
+    n = len(walls)
+    res = 0
+    for i in range(1, n - 1):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+        for j in range(i + 1, n):
+            right = max(right, walls[j])
+        res = res + (min(left, right) - walls[i])
+    return res
