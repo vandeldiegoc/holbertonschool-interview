25640efdeb06c69264a1a58de6c6439c3a219cec
vandel <vandeldiegoc@gmail.com>
2021-06-24 18:53:06 -0500

    0[add]

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""module"""
+
+
+def makeChange(coins, total):
+    """Given a pile of coins of different
+       values, determine the fewest number
+       of coins needed to meet a given amount total """
+    new = []
+    n = len(coins)
+    coins.sort()
+    coins = sorted(coins, reverse=True)
+    if total <= 0:
+        return 0
+    for i in range(n):
+        j = i
+        
+        while (j < n):
+
+            if sum(new) == total:
+                return new
+            if sum(new) < total:
+                new.append(coins[j])
+                
+                if sum(coins) % total == 0:
+                    j += 1
+                    continue
+
+
+            if sum(new) > total:
+                new.pop()
+                j += 1
+                continue
+    return -1  
