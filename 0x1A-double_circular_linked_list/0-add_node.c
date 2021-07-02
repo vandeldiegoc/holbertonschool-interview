1982ee158528be708ffd838d8ac07a86e68410d0
vandel <vandeldiegoc@gmail.com>
2021-07-01 23:05:01 -0500

    add new condition if linked list is NULL

--- 0x1A-double_circular_linked_list/0-add_node.c
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -8,50 +8,7 @@
  */
 List *add_node_end(List **list, char *str)
 {
-List *new_node, *tmp;
-
-if (list == NULL)
-		return (NULL);
-		
-if (str == NULL)
-	return (NULL);
-
-new_node = malloc(sizeof(list));
-
-if (new_node == NULL)
-	return (NULL);
-
-
-
-new_node->str = strdup(str);
-
-if (*list == NULL)
-{
-	new_node->next = new_node->prev = new_node;
-	*list = new_node;
-	return (new_node);
-}
-
-tmp = (*list)->prev;
-
-tmp->next = new_node;
-new_node->next = *list;
-new_node->prev = tmp;
-(*list)->prev = new_node;
-*list = new_node;
-return (new_node);
-}
-
-
-/**
- * add_node_end - Add a new node to the end of a double circular linked list: 
- * @list: list to be modify
- * @str: string to copy onto the new node
- * Return: Returns: Address of the new node, or NULL on failure
- */
-List *add_node_begin(List **list, char *str)
-{
-	List *new_node, *tmp;
+	List *new_node, *last_node;
 
 	if (!list)
 		return (NULL);
@@ -64,6 +21,52 @@ List *add_node_begin(List **list, char *str)
 		return (NULL);
 
 	new_node->str = strdup(str);
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	if (*list == NULL)
+	{
+		new_node->next = new_node->prev = new_node;
+		*list = new_node;
+		return (new_node);
+	}
+
+	last_node = (*list)->prev;
+
+	last_node->next = new_node;
+	new_node->next = *list;
+	new_node->prev = last_node;
+	(*list)->prev = new_node;
+	return (new_node);
+}
+
+
+/**
+ * add_node_end - Add a new node to the end of a double circular linked list:
+ * @list: list to be modify
+ * @str: string to copy onto the new node
+ * Return: Returns: Address of the new node, or NULL on failure
+ */
+List *add_node_begin(List **list, char *str)
+{
+	List *new_node, *tmp;
+
+	if (!list)
+		return (NULL);
+	if (!str)
+		return (NULL);
+	new_node = malloc(sizeof(List));
+	if (!new_node)
+		return (NULL);
+	new_node->str = strdup(str);
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
 	if (*list == NULL)
 	{
